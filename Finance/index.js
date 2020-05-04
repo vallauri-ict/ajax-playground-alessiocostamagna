19853ddd9b404e80afa915733047ae6b81c922f9
AlessioCostamagna <a.costamagna.0418@vallauri.edu>
2020-05-04 18:58:00 +0200

    update

--- Finance/index.js
+++ Finance/index.js
@@ -1,64 +1,79 @@
-$(document).ready(function(){
-    const apiKey="HBZ2XRR6ERZPKQ6D";
-    let json={
-        "Information Technology": "63.55%",
-        "Health Care": "31.83%",
-        "Consumer Discretionary": "26.19%",
-        "Utilities": "11.49%",
-        "Consumer Staples": "5.18%",
-        "Real Estate": "1.34%",
-        "Communication Services": "-4.45%",
-        "Materials": "-8.20%",
-        "Financials": "-8.90%",
-        "Industrials": "-9.83%",
-        "Energy": "-50.59%"
-    };
+$(document).ready(function () {
+    const apiKey = "HBZ2XRR6ERZPKQ6D";
+    let grafico;
 
-    
+    let chartOpt = $("#cmbChart");
+    let r = inviaRichiesta("GET", "http://localhost:3000/SECTOR");
+    r.done(function (data) {
+        for (let key in data) {
+            if (key != "Meta Data") {
+                let opt = $("<option>");
+                $(opt).val(key).text(key);
+                chartOpt.append(opt);
+            }
+        }
+    })
 
-    let table=$("#tblResults tbody");
+    let table = $("#tblResults tbody");
 
 
-    $("#CmbBrand").on("change",function(){
-        let symbol= $(this).val();
-        let r = inviaRichiesta("GET","http://localhost:3000/GLOBAL_QUOTE?symbol="+symbol);
-        r.done(function(data){
-            let brand=data[0]["Global Quote"];
+    $("#CmbBrand").on("change", function () {
+        let symbol = $(this).val();
+        let r = inviaRichiesta("GET", "http://localhost:3000/GLOBAL_QUOTE?symbol=" + symbol);
+        r.done(function (data) {
+            let brand = data[0]["Global Quote"];
             $(table).html("");
             $(table).append(createRow(brand));
         });
     });
-    
-    $("#txtSearch").on("keyup", function(){
-        if($(this).val().length<2)
+
+    $("#txtSearch").on("keyup", function () {
+        if ($(this).val().length < 2)
             return;
-        let r=inviaRichiesta("GET", "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + $(this).val() + "&apikey=" + apiKey);
-        r.done(function(data){
+        let r = inviaRichiesta("GET", "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + $(this).val() + "&apikey=" + apiKey);
+        r.done(function (data) {
             $(table).html("");
-            for(let i=0; i<5;i++)
-            {
-                let rSymbol=inviaRichiesta("GET","https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + data["bestMatches"][i]["1. symbol"] + "&apikey=" + apiKey);
-                rSymbol.done(function(data){
-                    let brand=data["Global Quote"];
+            for (let i = 0; i < 5; i++) {
+                let rSymbol = inviaRichiesta("GET", "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + data["bestMatches"][i]["1. symbol"] + "&apikey=" + apiKey);
+                rSymbol.done(function (data) {
+                    let brand = data["Global Quote"];
                     $(table).append(createRow(brand));
                 })
             }
         })
     })
+
+    $(chartOpt).on("change", function () {
+        let r = inviaRichiesta("GET", "http://localhost:3000/SECTOR");
+        r.done(function (data) {
+            let json = data[$(chartOpt).val()];
+            $("#myChart").remove();
+            let canvas = $("<canvas>");
+            $(canvas).prop("id","myChart").appendTo(".chartDiv");
+            setChart(json, canvas[0]);
+        })
+    })
+    $(chartOpt).trigger("change");
     
-    let chartr=inviaRichiesta("GET","http://localhost:3000/chart");
-    chartr.done(function(data){
-        let chartdata = data["data"];
-        chartdata["labels"] = [];
-        let datasets = chartdata["datasets"][0];
-        datasets["data"] = [];
-        for(let key in json)
-        {
-            chartdata["labels"].push(key);
-            datasets["data"].push(json[key].replace("%",""));
-            datasets["backgroundColor"].push("rgb(255,0,0)");
-        }
-        new Chart($("#myChart"),data);
+    function setChart(json, canvas) {
+        let chartr = inviaRichiesta("GET", "http://localhost:3000/chart");
+        chartr.done(function (data) {
+            let chartdata = data["data"];
+            chartdata["labels"] = [];
+            let datasets = chartdata["datasets"][0];
+            datasets["data"] = [];
+            for (let key in json) {
+                chartdata["labels"].push(key);
+                datasets["data"].push(json[key].replace("%", ""));
+                datasets["backgroundColor"].push("rgb(255,0,0)");
+            }
+            grafico = new Chart(canvas, data);
+        })
+    }
+
+    $("#imgChart").on("click",function(){
+        let url = grafico.toBase64Image();
+        $(this).prop("href",url);
     })
     function inviaRichiesta(method, url, parameters = "", async = true) {
         return $.ajax({ //PROMISE PER RICHESTA AJAX
